abbda1f770652f3cb892883e6c229fb871596e2c
avelezme <avelezme@chicagobooth.edu>
2021-11-03 23:08:23 +0000

    index works

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+
+#rule of thumb: one controller per database table!
+
+get("/directors", { :controller => "directors", :action => "index"})
+
+
+
+
+
 end
